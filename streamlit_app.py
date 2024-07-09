b4ffab98baab6b4ca5cbd4978105a28e0286f629
duende85 <121720880+duende85@users.noreply.github.com>
2024-07-09 01:37:42 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,7 +63,8 @@ if not st.session_state.logged_in:
             st.error('Invalid username or password')
 else:
     #st.write(f'Welcome, {st.session_state.username}!')
-    st.write('You are a data analyst at an e-commerce company. Your manager has asked you to analyze the orders placed by customers and respond several questions (find them at the end).','The data is stored in the tables named **customers** and **orders**. Below you can find in-built querying options (SQL-Lite).\n\n')
+    st.write('You are a data analyst at an e-commerce company. Your manager has asked you to analyze the orders placed by customers and respond several questions (find them at the end).\n\n'
+             'The data is stored in the tables named **customers** and **orders**. Below you can find in-built querying options (SQL-Lite).')
     # Create columns layout with custom widths
     col1, col2, col3 = st.columns([1.8, 2, 2.3])
 
